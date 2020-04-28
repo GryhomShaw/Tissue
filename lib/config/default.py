c37ea58b2a4cc8bcf94b6478dacccc5dee7a970f
GryhomShaw <xghongcs@163.com>
2020-04-28 17:45:59 +0800

    add dataset

--- lib/config/default.py
+++ lib/config/default.py
@@ -5,10 +5,11 @@ from yacs.config import CfgNode as CN
 
 _C = CN()
 _C.ROOT = '/home/gryhomshaw/SSD1T/xiaoguohong/MIL_Tissue'
+_C.RANDOMSEED = 2333
 
 #dataset
-
 _C.DATASET = CN()
+#gen_patch
 _C.DATASET.ROOT = '/home/gryhomshaw/SSD1T/xiaoguohong/MIL_Tissue/data'
 _C.DATASET.POS = os.path.join(_C.DATASET.ROOT, 'tissue-train-pos')
 _C.DATASET.NEG = os.path.join(_C.DATASET.ROOT, 'tissue-train-neg')
@@ -19,7 +20,10 @@ _C.DATASET.PATCHTHRESH = 0.65
 _C.DATASET.POOLSIZE = 1
 _C.DATASET.LOWWER = 20
 _C.DATASET.UPPER = 100
-
+_C.DATASET.MULTISCALE = [1,2]
+# tainval split
+_C.DATASET.SPLIT = os.path.join(_C.ROOT, 'lib/dataloader/train_val_split.json')
+_C.DATASET.SPLITRATIOS = [9, 1]
 
 
 def update_config(cfg, args):
